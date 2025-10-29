281a672639adfcfecdada90b9e7ee5a41c727808
fmihel <fmihel76@gmail.com>
2025-10-29 11:59:29 +0300

    fix

--- src/utils/Color.php
+++ src/utils/Color.php
@@ -66,5 +66,26 @@ class Color
         }
         return [0, 0, 0];
     }
+    public static function isHexColor($color): bool
+    {
+        $color  = strtolower($color);
+        $colors = str_split($color);
+        $count  = count($colors);
+
+        if ($count !== 7 && $count !== 9) {
+            return false;
+        }
+        if ($colors[0] !== '#') {
+            return false;
+        }
+
+        for ($i = 1; $i < $count; $i++) {
+            $code = ord($colors[$i]);
+            if (! (($code >= 97 && $code <= 102) || ($code >= 48 && $code <= 57))) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
